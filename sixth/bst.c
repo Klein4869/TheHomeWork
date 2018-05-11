50d874347e27d701eb5aa31b38888e293c6ffde5
thd <thd1998@live.cn>
2018-05-11 12:52:41 +0800

    []

--- sixth/bst.c
+++ sixth/bst.c
@@ -1,19 +1,18 @@
-#include <iostream>
+
 #include "stdio.h"
+#include "string.h"
 
 int is_find = 0;
 
-struct treenode
-{
+struct treenode {
     int depth;
     struct treenode *left;
     struct treenode *right;
     int value;
 };
 
-void insert(struct treenode *node, int temp, int depth){
-    if (node->depth == 0)
-    {
+void insert(struct treenode *node, int temp, int depth) {
+    if (depth == 0) {
         node->value = temp;
         return;
     }
@@ -23,56 +22,64 @@ void insert(struct treenode *node, int temp, int depth){
     node->right = NULL;
 }
 
-void find(struct treenode *node, int temp){
-    if (is_find == 1)
-    {
+
+void find(struct treenode *node, int temp) {
+    if (is_find == 1) {
         return;
     }
 
-    if (node->value == 0)
-    {
+    if (node->value == -25765) {
         insert(node, temp, 0);
         return;
     }
-    if (temp >= node->value)
-    {
-        if (node->right == NULL)
-        {
-            struct treenode n;
-            node->right = &n;
-            insert(node->right, temp, node->depth+1);
+    if (temp >= node->value) {
+        if (node->right == NULL) {
+            struct treenode *n = (struct treenode *) malloc(sizeof(struct treenode));
+            node->right = n;
+            insert(node->right, temp, node->depth + 1);
             is_find = 1;
             return;
-        }else{
+        } else {
             find(node->right, temp);
         }
-    }else{
-        if (node->left == NULL)
-        {
-            struct treenode n;
-            node->left = &n;
-            insert(node->left, temp, node->depth+1);
+    } else {
+        if (node->left == NULL) {
+            struct treenode *n = (struct treenode *) malloc(sizeof(struct treenode));
+            node->left = n;
+            insert(node->left, temp, node->depth + 1);
             is_find = 1;
             return;
-        }else{
+        } else {
             find(node->left, temp);
         }
     }
 }
 
 
-int main(int argc, char const *argv[])
-{
+void find_leaves(struct treenode *node){
+    if (node->left == NULL && node->right == NULL){
+        printf("%d %d\n", node->value, node->depth+1);
+    }
+    if (node->left != NULL){
+        find_leaves(node->left);
+    }
+    if (node->right != NULL){
+        find_leaves(node->right);
+    }
+}
+
+
+int main(int argc, char const *argv[]) {
     struct treenode headnode;
     struct treenode *h;
     h = &headnode;
-    headnode.value = 0;
+    headnode.value = -25765;
     headnode.depth = 0;
     headnode.left = NULL;
     headnode.right = NULL;
     int num;
     scanf("%d", &num);
-    while(num--){
+    while (num--) {
         int temp;
         scanf("%d", &temp);
         // if (max_depth == 0)
@@ -88,5 +95,6 @@ int main(int argc, char const *argv[])
         // tmp.value = temp;
         find(h, temp);
     }
+    find_leaves(h);
     return 0;
 }
