bf6bbbcd63e496640cee3fa9a60ca4c232a3c026
Klein4869 <thd1998@live.cn>
2018-05-14 09:45:43 +0800

    []

--- sixth/finder.c
+++ sixth/finder.c
@@ -1,30 +1,55 @@
-#include <malloc.h>
+#include "stdlib.h"
 #include "stdio.h"
 #include "string.h"
 
-struct family_tree {
+struct family_tree
+{
     char name[30];
     int depth;
     family_tree *left;
     family_tree *right;
 };
 
+char name[5][30];
+
 int is_find = 0;
+int max;
+int flag = 1;
 
-void insert(struct family_tree *node, char temp[]) {
-
+void insert(struct family_tree *node, char temp[])
+{
 }
 
-int find(struct family_tree *node, char temp[]) {
-    if (is_find == 1) {
+int find(struct family_tree *node, char temp[], struct family_tree *f)
+{
+    if (node == NULL)
+    {
+        node = f;
         return 1;
     }
-
-    if (!strcmp(node->name, head.name))
-
+    if (is_find == 1)
+    {
+        return 1;
+    }
+    if (!strcmp(node->name, temp))
+    {
+        is_find = 1;
+        node = f;
+        return 1;
+    }
+    else
+    {
+        find(node->left, temp, f);
+        find(node->right, temp, f);
+    }
+    if (is_find == 1)
+    {
+        return 1;
+    }
 }
 
-int main() {
+int main()
+{
     char file_in_name[] = "in.txt";
     FILE *fp = fopen(file_in_name, "r");
     int kided_number;
@@ -33,44 +58,62 @@ int main() {
     family_tree head;
     family_tree *h;
     h = &head;
-    while (kided_number--) {
-        char name[30];
+    while (kided_number--)
+    {
+        int is_first = 1;
         char temp[200];
+        char senpai[30];
         fgets(temp, 200, fp);
         family_tree *f = (family_tree *)malloc(sizeof(family_tree));
         int i = 0;
         int j = 0;
-        int flag = 1;
-        while (temp[i] != '\0') {
-            if (temp[i] == ' ') {
-                name[j] = '\0';
+        int k = 0;
+        while (temp[i] != '\0')
+        {
+            if (temp[i] == ' ')
+            {
+                name[k++][j] = '\0';
                 j = 0;
                 i++;
-                //第一个人特殊处理
-                if (flag == 1) {
-                    strcpy(head.name, name);
-                    head.depth = 0;
-                    flag = 0;
-                }
-                //
-                //不是第一个人的情况
-                strcpy(f->name, name);
-                //
-                if (find(h, name));
-                    continue;
+                continue;
             }
-            name[j++] = temp[i];
+            name[k][j++] = temp[i];
             i++;
         }
-        name[j] = '\0';
-        //考虑家族中只有一人情况
-        if (flag == 1){
-            strcpy(head.name, name);
-            head.depth = 0;
-            flag = 0;
+        name[k++][j] = '\0';
+        //        //考虑家族中只有一人情况
+        //        if (flag == 1){
+        //            strcpy(head.name, name[k-1]);
+        //            head.depth = 0;
+        //            flag = 0;
+        //        }
+        //        //
+        max = k;
+        struct family_tree *f_left = (family_tree *)malloc(sizeof(family_tree));
+        struct family_tree *f_right = (family_tree *)malloc(sizeof(family_tree));
+        if (max == 3)
+        {
+            if (!strcmp("NULL", name[2]))
+            {
+                f->left = f_left;
+                f->right = NULL;
+                strcpy(f_left->name, name[1]);
+            }
+            f->left = f_left;
+            f->right = f_right;
+            strcpy(f_left->name, name[1]);
+            strcpy(f_right->name, name[2]);
         }
-        //
-        if (find())
+        if (max == 1)
+        {
+            f->left = NULL;
+            f->right = NULL;
+        }
+        if (find(h, name[0], f))
+        {
+        }
+        getchar();
     }
+    fclose(fp);
     return 0;
 }
