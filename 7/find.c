f50674aeb1d17c1e6d7a2c4b8db125c2e2e4a900
Klein4869 <thd1998@live.cn>
2018-06-02 00:23:29 +0800

    df

---
+++ 7/find.c
@@ -0,0 +1,72 @@
+#include <string.h>
+#include "stdio.h"
+
+int is_find = 0;
+int time = 0;
+
+char words[3600][30];
+
+void bisearch(int first, int last, char *b){
+    if(is_find){
+        return;
+    }
+    if (last == first){
+        time++;
+        if (!strcmp(words[first], b)){
+            printf("1 %d", time);
+            is_find = 1;
+            return;
+        } else{
+            printf("0 %d", time);
+            is_find = 1;
+            return;
+        }
+    }
+    int mid = (first+last)/2;
+    time++;
+    if (!strcmp(words[mid], b)){
+        printf("1 %d", time);
+        is_find = 1;
+        return;
+    }else{
+        if(strcmp(words[mid], b) > 0)
+            bisearch(first, mid-1, b);
+        else
+            bisearch(mid+1, last, b);
+    }
+}
+
+int main() {
+    int count = 0, i, compare_num = 0;
+    FILE *fp = fopen("/Users/haidongtang/ctest/cmake-build-debug/dictionary3500.txt", "r");
+    while (!feof(fp))
+        fscanf(fp, "%s", words[count++]);
+    char word[30];
+    scanf("%s", word);
+    char method;
+    method = getchar();
+    while (method == ' '){
+        method = getchar();
+    }
+    if (method == '1') {
+        for (i = 0; i < count; ++i) {
+            if (!strcmp(word, words[i]) || strcmp(words[i], word) > 0) {
+                printf("1 %d\n", ++compare_num);
+                break;
+            }
+            else
+                compare_num++;
+
+        }
+        if (strcmp(word, word[i]) != 0)
+            printf("0 %d", compare_num);
+    }
+    if (method == '2'){
+        bisearch(0, count, word);
+        if(!is_find){
+            printf("0 %d", time);
+        }
+    }
+}
+
+
