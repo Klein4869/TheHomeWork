eca67cbf6656d76763db50743c1c6300f506f656
Klein4869 <thd1998@live.cn>
2018-06-07 14:25:57 +0800

    1

--- 7/find.c
+++ 7/find.c
@@ -6,6 +6,44 @@ int time = 0;
 
 char words[3600][30];
 
+struct hash_table{
+    int next;
+    char word[30];
+}hash_table1[100000];
+
+/* compute hash value for string */
+
+#define NHASH  3001
+
+#define MULT  37
+
+unsigned int hash(char *str)
+{
+    unsigned int h=0;
+    char *p;
+    for(p=str; *p!='\0'; p++)
+    h = MULT*h + *p;
+    return h % NHASH;
+}
+
+void create_hash(char word[30]){
+    int hash_code = hash(word);
+    int temp = 100000 - 1;
+    if(!strcmp(hash_table1[hash_code].word, "0")){
+        strcpy(hash_table1[hash_code].word, word);
+    }else{
+        while(strcmp(hash_table1[temp].word, "0")){
+            temp--;
+        }
+        while (hash_table1[hash_code].next != 0) {
+            hash_code = hash_table1[hash_code].next;
+        }
+        hash_table1[hash_code].next = temp;
+        strcpy(hash_table1[temp].word, word);
+    }
+}
+
+
 int bisearch(int first, int last, char *b){
     if(first > last)
         return -1;
@@ -21,17 +59,22 @@ int bisearch(int first, int last, char *b){
 
 int main() {
     int count = 0, i, compare_num = 0;
+    for (i = 0; i < 100000; ++i){
+        strcpy(hash_table1[i].word, "0");
+        hash_table1[i].next = 0;
+    }
     int first[26];
     int last[26];
-    FILE *fp = fopen("/Users/haidongtang/ctest/cmake-build-debug/dictionary3500.txt", "r");
+    FILE *fp = fopen("dictionary3000.txt", "r");
     char temp = '0';
     while (!feof(fp)) {
         fscanf(fp, "%s", words[count++]);
-        if (words[count - 1] != temp){
-            first[(int)(words[count-1] - 'a')] = count - 1;
+        create_hash(words[count-1]);
+        if (words[count - 1][0] != temp){
+            first[words[count-1][0] - 'a'] = count - 1;
             last[temp - 'a'] = count - 2;
         }
-        temp = words[count - 1];
+        temp = words[count - 1][0];
     }
     char word[30];
     scanf("%s", word);
@@ -42,29 +85,51 @@ int main() {
     }
     if (method == '1') {
         for (i = 0; i < count; ++i) {
-            if (!strcmp(word, words[i]) || strcmp(words[i], word) > 0) {
-                printf("1 %d\n", ++compare_num);
+            compare_num++;
+            if (strcmp(words[i], word) >= 0) {
                 break;
             }
-            else
-                compare_num++;
-
         }
-        if (strcmp(word, word[i]) != 0)
-            printf("0 %d", compare_num);
+        if (strcmp(word, words[i]) != 0)
+            printf("0 %d\n", compare_num);
+        else
+            printf("1 %d\n", compare_num);
     }
     if (method == '2'){
-        int ans = bisearch(0, count, word);
+        int ans = bisearch(0, count-1, word);
         if(ans == -1){
-            printf("0 %d", time);
+            printf("0 %d\n", time);
         } else{
-            printf("1 %d", time);
+            printf("1 %d\n", time);
         }
     }
     if (method == '3'){
-        
+        int ans = bisearch(first[word[0] - 'a'], last[word[0] - 'a'], word);
+        if(ans == -1){
+            printf("0 %d\n", time);
+        } else{
+            printf("1 %d\n", time);
+        }
+    }
+    if (method == '4'){
+        int word_hash = hash(word);
+        while(strcmp(hash_table1[word_hash].word, "0")){
+            time++;
+            if (!strcmp(word, hash_table1[word_hash].word))
+            {
+                printf("1 %d\n", time);
+                break;
+            }
+            if (strcmp(word, hash_table1[word_hash].word) < 0)
+            {
+                printf("0 %d\n", time);
+                break;
+            }
+            word_hash = hash_table1[word_hash].next;
+            if (word_hash == 0) {
+                printf("0 %d\n", time);
+                break;
+            }
+        }
     }
 }
-
-
-
