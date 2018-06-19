521260d477d47dc3c144fc24a8bf68a45161f4e1
klein4869 <thd1998@live.cn>
2018-06-20 02:43:01 +0800

    1

--- synthesis/markov.c
+++ synthesis/markov.c
@@ -1,101 +1,117 @@
 #include "string.h"
 #include "stdio.h"
 #include "stdlib.h"
+#include "math.h"
 
-char input[5000000];
-char *word[100000];
+char input[10000000];
+char *word[5000000];
 int wordNum = 0;
 int k = 2;
-int next[100000];
-int bin[50000];
+int next[5000000];
+int bin[1500009];
+int N[1500009] = { 0 };
+
+double seed = 997;
+
+double rrand()
+{
+	double lambda = 3125.0;
+	double m = 34359738337.0;
+	double r;
+	seed = fmod(lambda * seed, m); //要包含头文件#include <math.h>
+	r = seed / m;
+	return r;
+}
 
 unsigned int hash(char *str)
 {
-    int n;
-    unsigned int h = 0;
-    char *p = str;
-    for (n = k; n > 0; ++p)
-    {
-        h = 31 * h + *p;
-
-        if (*p == '\0')
-        {
-            --n;
-        }
-    }
-    return h % 50000;
+	int n;
+	unsigned int h = 0;
+	char *p = str;
+	for (n = k; n > 0; ++p)
+	{
+		h = 31 * h + *p;
+		if (*p == '\0')
+		{
+			--n;
+		}
+	}
+	return h % 1500009;
 }
 
-int wordCmp(char *p, char *q)
+int wordCmp(int i, int j)
 {
-    int i;
-
-    for (i = k; *p == *q; p++, q++)
-        if (*p == '\0' && (--i) == 0)
-            return 0;
-
-    return *p - *q;
+	int l;
+	if(!strcmp(word[i], word[j]) && !strcmp(word[i+1], word[j+1]))
+		return 0;
+	return -1;
 }
 
 char *skip(char *p, int n)
 {
-    for (; n > 0; p++)
-        if (*p == '\0')
-            --n;
-    return p;
+	for (; n > 0; p++)
+		if (*p == '\0')
+			--n;
+	return p;
 }
 
 int main(int argc, char const *argv[])
 {
-    int i, j;
-    word[0] = input;
+	int i, j;
+	word[0] = input;
 
-    FILE *fp = fopen("article.txt", "r");
+	FILE *fp = fopen("article.txt", "r");
 
-    while (!feof(fp))
-    {
-        fscanf(fp, "%s", word[wordNum]);
-        word[wordNum + 1] = word[wordNum] + strlen(word[wordNum]) + 1;
-        wordNum++;
-    }
-    for (i = 0; i < 50000; i++)
-        bin[i] = -1;
-    for (i = 0; i < wordNum - k; i++)
-    {
-        j = hash(word[i]);
-        next[i] = bin[j];
-        bin[j] = i;
-    }
+	while (!feof(fp))
+	{
+		fscanf(fp, "%s", word[wordNum]);
+		word[wordNum + 1] = word[wordNum] + strlen(word[wordNum]) + 1;
+		wordNum++;
+	}
+	memset(bin, -1, sizeof(bin));
+	for (i = 0; i <= wordNum - k; i++)
+	{
+		j = hash(word[i]);
+		next[i] = bin[j];
+		int temp = i;
+		while (next[temp] != -1)
+			temp = next[temp];
+		next[temp] = i;
+		next[i] = -1;
+		if (bin[j] == -1)
+			bin[j] = temp;
+	}
 
-    int wordsleft;
-    int psofar;
-    char *phrase, *p;
-    phrase = input;
+	int wordsleft, n;
+	char *phrase, *p;
+	phrase = input;
 
-    FILE *fp1 = fopen("markov.txt", "a+");
+	FILE *fp1 = fopen("markov.txt", "w");
 
-    fprintf(fp1, "%s %s ", skip(phrase, 0), skip(phrase, 1));
+	fprintf(fp1, "%s %s ", word[0], word[1]);
 
-    for (wordsleft = 1000000; wordsleft > 0; --wordsleft)
-    {
-        psofar = 0;
+	int max = 100;
+	scanf("%d", &max);
+	int phrase_index = 0;
 
-        for (j = bin[hash(phrase)]; j >= 0; j = next[j])
-        {
-
-            if (wordCmp(phrase, word[j]) == 0 && rand() % ++psofar == 0)
-            {
-                p = word[j];
-            }
-            phrase = skip(p, 1);
-
-            if (strlen(skip(phrase, k - 1)) == 0)
-            {
-                break;
-            }
-            fprintf(fp1, "%s ", skip(phrase, k - 1));
-        }
-    }
-
-    return 0;
+	for (wordsleft = max; wordsleft > 0; --wordsleft)
+	{
+		n = 0;
+		for (j = bin[hash(phrase)]; j >= 0; j = next[j]) {
+			if (wordCmp(phrase_index, j) == 0)
+				n++;
+		}
+		if (n == 1) n = 0;
+		else n = (int)(rrand() * n);
+		for (j = bin[hash(phrase)]; j >= 0; j = next[j]) {
+			if (wordCmp(phrase_index, j) == 0 && n-- == 0)
+				break;
+		}
+		phrase = word[j+1];
+		phrase_index = j+1;
+		if (strlen(word[j+2]) == 0)
+			break;
+		fprintf(fp1, "%s ", word[j+2]);
+	}
+	return 0;
 }
