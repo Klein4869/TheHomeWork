3fabd5181665f0a9ed42b6ed70f93692ed37444e
klein4869 <thd1998@live.cn>
2018-06-21 01:49:17 +0800

    1

--- synthesis/markov.c
+++ synthesis/markov.c
@@ -8,8 +8,9 @@ char *word[5000000];
 int wordNum = 0;
 int k = 2;
 int next[5000000];
-int bin[1500009];
-int N[1500009] = { 0 };
+int bin[13131313];
+char *wordBin[13131313];
+int N[13131313];
 
 double seed = 997;
 
@@ -23,26 +24,35 @@ double rrand()
 	return r;
 }
 
-unsigned int hash(char *str)
+unsigned long long hash(char *str)
 {
+	int a = 10971;
 	int n;
-	unsigned int h = 0;
+	unsigned long long h = 0;
 	char *p = str;
 	for (n = k; n > 0; ++p)
 	{
-		h = 31 * h + *p;
+		h = h * a + *p;
 		if (*p == '\0')
-		{
 			--n;
-		}
 	}
-	return h % 1500009;
+	return (h % 13131313);
 }
 
-int wordCmp(int i, int j)
+int wordCmp(char *p, char *q)
 {
-	int l;
-	if(!strcmp(word[i], word[j]) && !strcmp(word[i+1], word[j+1]))
+	int n;
+	for (n = k; *p == *q; ++p, ++q)
+	{
+		if (*p == '\0' && (--n) == 0)
+			return 0;
+	}
+	return *p - *q;
+}
+
+int wordCmp1(int i, int j)
+{
+	if (!strcmp(word[i], word[j]) && !strcmp(word[i + 1], word[j + 1]))
 		return 0;
 	return -1;
 }
@@ -57,7 +67,7 @@ char *skip(char *p, int n)
 
 int main(int argc, char const *argv[])
 {
-	int i, j;
+	long long i, j;
 	word[0] = input;
 
 	FILE *fp = fopen("article.txt", "r");
@@ -72,6 +82,8 @@ int main(int argc, char const *argv[])
 	for (i = 0; i <= wordNum - k; i++)
 	{
 		j = hash(word[i]);
+		while (wordBin[j] != NULL && wordCmp(word[i], wordBin[j]) != 0)
+			j++;
 		next[i] = bin[j];
 		int temp = i;
 		while (next[temp] != -1)
@@ -79,10 +91,14 @@ int main(int argc, char const *argv[])
 		next[temp] = i;
 		next[i] = -1;
 		if (bin[j] == -1)
+		{
 			bin[j] = temp;
+			wordBin[j] = word[i];
+		}
+		N[j]++;
 	}
 
-	int wordsleft, n;
+	int wordsleft, n, m;
 	char *phrase, *p;
 	phrase = input;
 
@@ -90,28 +106,41 @@ int main(int argc, char const *argv[])
 
 	fprintf(fp1, "%s %s ", word[0], word[1]);
 
-	int max = 100;
+	int max = 100, num = 0, shit, num1 = 0;
 	scanf("%d", &max);
-	int phrase_index = 0;
+	int chongtu = 0;
 
 	for (wordsleft = max; wordsleft > 0; --wordsleft)
 	{
-		n = 0;
-		for (j = bin[hash(phrase)]; j >= 0; j = next[j]) {
-			if (wordCmp(phrase_index, j) == 0)
-				n++;
-		}
-		if (n == 1) n = 0;
-		else n = (int)(rrand() * n);
-		for (j = bin[hash(phrase)]; j >= 0; j = next[j]) {
-			if (wordCmp(phrase_index, j) == 0 && n-- == 0)
+		m = hash(phrase);
+		while (wordCmp(phrase, wordBin[m]) != 0)
+			m++;
+		n = N[m];
+		// m = hash(phrase);
+		// for (j = bin[hash(phrase)]; j >= 0; j = next[j])
+		// {
+		// 	if (wordCmp(phrase, word[j]) == 0)
+		// 		n++;
+		// }
+		if (n == 1)
+			n = 0;
+		else
+			n = (int)(rrand() * n);
+		for (j = bin[m]; j >= 0; j = next[j])
+		{
+			if (n-- == 0)
+			{
+				p = word[j];
 				break;
+			}
 		}
-		phrase = word[j+1];
-		phrase_index = j+1;
-		if (strlen(word[j+2]) == 0)
+		phrase = skip(p, 1);
+		if (strlen(skip(phrase, k - 1)) == 0)
 			break;
-		fprintf(fp1, "%s ", word[j+2]);
+		fwrite(skip(phrase, k - 1), strlen(skip(phrase, k - 1)), 1, fp1);
+		fwrite(" ", 1, 1, fp1);
+		// fprintf(fp1, "%s ", word[j+2]);
 	}
+	//fprintf(fp1, "%d ", num);
 	return 0;
 }
